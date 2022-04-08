377cbfafc1e860df6b4d06aea94c763022efe9e9
Alisson Menezes <Alisson.itops@gmail.com>
2022-04-08 18:37:50 +0200

    managing groups

--- main.tf
+++ main.tf
@@ -13,6 +13,11 @@ data azurerm_key_vault "kv"{
     resource_group_name = var.resource_group_name
 }
 
+data "azuread_user" "users" {
+  for_each = var.data_experts
+  user_principal_name = each.key
+}
+
 resource "azurerm_role_assignment" "df_to_sa" {
   scope                = data.azurerm_storage_account.sa.id
   role_definition_name = "Storage Blob Data Contributor"
@@ -27,5 +32,20 @@ resource "azurerm_key_vault_access_policy" "df_to_kv" {
   secret_permissions = [
     "Get",
     "List"
+  ]
+}
+
+resource "azuread_group" "de_group" {
+  display_name     = "RL_ADP_${var.project_name}_DX"
+  owners           = ["6d574035-a99d-4cd7-b7e6-403b7fe849ad"]
+  security_enabled = true
+}
+
+resource "azuread_group_member" "de" {
+  for_each = data.azuread_user.users
+  group_object_id  = azuread_group.de_group.id
+  member_object_id = each.value.id
+  depends_on = [
+    azuread_group.de_group
   ]
 }
